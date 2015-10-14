0f8651770ca1165107e59a48b481c9b44e5e8c18
Sahil Sharma <ssharma@squiz.net>
2015-10-14 16:59:45 +1100

    Updating Config.php

--- src/Pyntax/Config/Config.php
+++ src/Pyntax/Config/Config.php
@@ -33,7 +33,7 @@ class Config extends ConfigAbstract
     /**
      * @var bool
      */
-    protected $_path = false;
+    protected static $_path = false;
 
     /***
      * @param bool|false $defaultKey
@@ -44,7 +44,7 @@ class Config extends ConfigAbstract
     {
         $this->_default_key = $defaultKey;
         if (!empty($path)) {
-            $this->_path = $path;
+            self::$_path = $path;
         }
         $this->loadConfig($fileToBeLoaded);
     }
@@ -55,19 +55,19 @@ class Config extends ConfigAbstract
      */
     protected function loadConfig($filesToBeLoaded = "config.php")
     {
-        if (empty($this->_path)) {
-            $this->_path = dirname(realpath(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . self::$_config_folder_name)) . DIRECTORY_SEPARATOR . self::$_config_folder_name;
+        if (empty(self::$_path)) {
+            self::$_path = dirname(realpath(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . self::$_config_folder_name)) . DIRECTORY_SEPARATOR . self::$_config_folder_name;
         }
 
         if (empty($filesToBeLoaded)) {
             //Do not load anything if the file is empty
         } else if (is_array($filesToBeLoaded)) {
             foreach ($filesToBeLoaded as $_fileToBeLoaded) {
-                include_once $this->_path . DIRECTORY_SEPARATOR . $_fileToBeLoaded;
+                include_once self::$_path . DIRECTORY_SEPARATOR . $_fileToBeLoaded;
             }
         } else if (!empty($filesToBeLoaded)) {
-            if (file_exists($this->_path . DIRECTORY_SEPARATOR . $filesToBeLoaded)) {
-                include_once $this->_path . DIRECTORY_SEPARATOR . $filesToBeLoaded;
+            if (file_exists(self::$_path . DIRECTORY_SEPARATOR . $filesToBeLoaded)) {
+                include_once self::$_path . DIRECTORY_SEPARATOR . $filesToBeLoaded;
             }
         }
     }
